c934177ed5e0718a8302fbcce0e88b143e6213dd
d-beloved <morayodeji@gmail.com>
2021-01-09 14:53:22 +0100

    setup server barebone

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import logger from 'morgan';
+import cors from 'cors';
+
+
+// Set up the express app
+const app = express();
+app.use((req, res, next) => {
+  setTimeout(() => {
+    next();
+  }, 2000);
+});
+// set the port for the server
+const port = process.env.PORT || 3110;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Cospik api is running on port ${port}!`);
+});
+
+// Parse incoming requests data
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use('/api-docs', express.static(path.join(__dirname, '../build')));
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// set router for api endpoints
+// app.use('/api/', routes);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../nativeClient/index.html'));
+// });
+
+// This will be our application entry. Our server is setup here.
+
+export default app;
