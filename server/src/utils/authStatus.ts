7cb1d847f83438b162650611cb75972a131b8991
d-beloved <morayodeji@gmail.com>
2021-01-11 19:45:56 +0100

    protect the endpoints

--- server/src/utils/authStatus.ts
+++ server/src/utils/authStatus.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const secretKey = process.env.JWT_SECRET;
 
 
-// checks if a token was passed into the request header
+// checks if a user is already logged in
 export const loginStatus = (req, res, next) => {
   if (req.headers.authorization) {
     try {
@@ -21,3 +21,23 @@ export const loginStatus = (req, res, next) => {
     next();
   };
 }
+
+// Authenticates every action to be carried out on the app and
+// checks the validity of the token
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).send({ message: 'token is required!', success: false });
+  } else {
+    // checks if token matches the one provided at login
+    const rightToken = token.split(' ')[1]; // Splits the token to reveal the user
+    jwt.verify(rightToken, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).send({ message: 'Authentication failed! Token is Invalid or expired. Please Login again', success: false });
+      } else {
+        req.userData = decoded.username;
+        next();
+      }
+    });
+  }
+};
