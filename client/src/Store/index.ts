bddadc82e36bf74f8ff23b722864b4a7087df1b8
d-beloved <morayodeji@gmail.com>
2021-01-18 15:44:05 +0100

    add MIT License

--- client/src/Store/index.ts
+++ client/src/Store/index.ts
@@ -1,43 +1,6 @@
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import { createLogger } from "redux-logger";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import reducers from "./reducers";
-
-// const loggerMiddleware = () => {
-//   if (process.env.NODE_ENV === "development") {
-//     const logger = createLogger({
-//       collapsed: true,
-//     });
-//     return [logger];
-//   }
-
-//   return [];
-// };
-// const configureStore = () => {
-//   const composedEnhancers = composeWithDevTools(
-//     applyMiddleware(thunk, ...loggerMiddleware())
-//   );
-
-//   return createStore(reducers, composedEnhancers);
-// };
-
-// export default configureStore;
-
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-
-// const loggerMiddleware = () => {
-//   if (process.env.NODE_ENV === "development") {
-//     const logger = createLogger({
-//       collapsed: true,
-//     });
-//     return [logger];
-//   }
-
-//   return [];
-// };
 
 let composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [thunk];
